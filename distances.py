7988a08e4a107794fcb744c0dbc705dd18840d45
Poul Hornsleth <poulh@umich.edu>
2020-03-22 13:30:59 -0400

    read county data from url instead of file

--- distances.py
+++ distances.py
@@ -2,14 +2,16 @@
 import pandas as pd
 import json
 import math
+import io
+import requests
 
 EARTH_RADIUS_MI = 3961
 EARTH_RADIUS_KM = 6373
 
 # https://data.healthcare.gov/dataset/Geocodes-USA-with-Counties/52wv-g36k
 
-
-def distance(radius):
+  
+def distance(radius):  
     def dist(lat1, lon1, lat2, lon2):
         dlat = math.radians(lat2 - lat1)
         dlon = math.radians(lon2 - lon1)
@@ -25,9 +27,34 @@ def distance(radius):
 distance_miles = distance(EARTH_RADIUS_MI)
 distance_km = distance(EARTH_RADIUS_KM)
 
+
+
+def load_county_data():
+    url = "https://data.healthcare.gov/api/views/52wv-g36k/rows.tsv?accessType=DOWNLOAD&sorting=true"
+    content = requests.get(url).content.decode('utf-8')
+    counties = pd.read_csv(io.StringIO(content), sep='\t')
+
+    counties = counties[counties['decommissioned'] == 0]
+    counties = counties[counties['estimated_population'] != 0]
+    counties['county'] = counties['county'].str.replace(' Borough','')
+    counties['county'] = counties['county'].str.replace(' Census Area','')
+
+    counties['latitude'] = counties['latitude'] * counties['estimated_population']
+    counties['longitude'] = counties['longitude'] * counties['estimated_population']
+
+    counties = counties.groupby(['state','county'])[['latitude','longitude','estimated_population']].apply(sum).reset_index()
+
+    counties['latitude'] = counties['latitude'] / counties['estimated_population']
+    counties['longitude'] = counties['longitude'] / counties['estimated_population']
+    
+    return counties
+
+
+
+
 teams = pd.read_csv('teams.tsv', sep='\t').set_index('Franchise')
 
-counties = pd.read_csv('county_populations_lat_long.tsv', sep='\t')
+counties = load_county_data()
 
 distances = pd.DataFrame({
     'County':
@@ -41,10 +68,10 @@ results = pd.DataFrame({
 results.set_index('County', inplace=True)
 
 for index, team in teams.iterrows():
-    print(team.name)
+    print(team.name) 
 
-    distances[team.name] = counties.apply(lambda county: dist(
-        county['lat'], county['lon'], team['Latitude'], team['Longitude']),
+    distances[team.name] = counties.apply(lambda county: distance_miles(
+        county['latitude'], county['longitude'], team['Latitude'], team['Longitude']),
                                           axis=1)
 
 distances.set_index('County', inplace=True)
@@ -55,36 +82,38 @@ results.reset_index(inplace=True)
 
 grouped = results.groupby('Closest')['County'].apply(list)
 
-mapdata = {
-    "title": "",
-    "hidden": [],
-    "background": "#ffffff",
-    "borders": "#000000"
-}
+print(grouped)
+exit()
+# mapdata = {
+#     "title": "",
+#     "hidden": [],
+#     "background": "#ffffff",
+#     "borders": "#000000"
+# }
 
-groups = {}
+# groups = {}
 
-counter = 0
-for index, team in teams.iterrows():
-    print(team.name)
-    try:
+# counter = 0
+# for index, team in teams.iterrows():
+#     print(team.name)
+#     try:
 
-        obj = {
-            "div": "#box{}".format(counter),
-            "label": "",
-            "paths": grouped[team.name]
-        }
-        groups[team['Primary']] = obj
-        counter += 1
-        print(counter)
-    except:
-        print("error.")
-        pass
+#         obj = {
+#             "div": "#box{}".format(counter),
+#             "label": "",
+#             "paths": grouped[team.name]
+#         }
+#         groups[team['Primary']] = obj
+#         counter += 1
+#         print(counter)
+#     except:
+#         print("error.")
+#         pass
 
-mapdata["groups"] = groups
+# mapdata["groups"] = groups
 
-with open('mapdata.json', 'w') as outfile:
-    json.dump(mapdata, outfile, indent=2)
+# with open('mapdata.json', 'w') as outfile:
+#     json.dump(mapdata, outfile, indent=2)
 
 #res2.reset_index()
 #res2.rename({"1": "Counties"}, axis=1, inplace=True)
